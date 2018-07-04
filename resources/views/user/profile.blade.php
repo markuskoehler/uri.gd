6f7fc279d89b9850dbf1a7cd6ba9d472a1875f9b
Markus Koehler <info@koehler.pro>
2018-07-04 21:48:16 +0200

    add icons on dashboard

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -10,10 +10,10 @@
                     <div class="card-body">
                         {{ __('You currently have :count URLs saved.', ['count' => $count]) }}
                             @if($count)
-                            <a class="btn btn-info" href="{{ url()->route('urls.index') }}">{{ __('View them') }}</a>
+                            <a class="btn btn-info" href="{{ url()->route('urls.index') }}"><i class="fas fa-eye"></i> {{ __('View them') }}</a>
                             @endif
                             <br><br>
-                        <a class="btn btn-primary" href="{{ url()->route('urls.create') }}">{{ __('Create new') }}</a>
+                        <a class="btn btn-primary" href="{{ url()->route('urls.create') }}"><i class="fas fa-plus-circle"></i> {{ __('Create new') }}</a>
                     </div>
                 </div>
             </div>
