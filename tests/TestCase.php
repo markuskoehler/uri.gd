2eb0268f8d364ddb3985dc4c87943edf2c6738af
Markus Koehler <info@koehler.pro>
2018-07-29 00:17:08 +0200

    test macro to follow redirects

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,26 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\TestResponse;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        //$this->withoutExceptionHandling();
+
+        TestResponse::macro('followRedirects', function ($testCase) {
+            $response = $this;
+
+            while ($response->isRedirect()) {
+                $response = $testCase->get($response->headers->get('Location'));
+            }
+
+            return $response;
+        });
+    }
 }
