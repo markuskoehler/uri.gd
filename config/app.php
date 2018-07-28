f72bb993265f1e11d8c48f1b88e35e4644e09dea
Markus Koehler <info@koehler.pro>
2018-07-28 20:54:16 +0200

    fixing localization facade for travis and some more options added

--- config/app.php
+++ config/app.php
@@ -210,6 +210,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'LaravelLocalization' => Mcamara\LaravelLocalization\Facades\LaravelLocalization::class,
 
     ],
 
